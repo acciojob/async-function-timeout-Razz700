27ab8224da3f69ef4c0b534b91727b02e82c52bc
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-13 17:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let input1=document.getElementById('text');
+let input2=document.getElementById('delay');
+let btn=document.getElementById('btn');
+let out=document.getElementById('output');
+btn.addEventListener('click',()=>{
+	out.innerHTML="";
+	const p1=new Promise((a,b)=>{
+	setTimeout(()=>{
+		a('resolved');
+	},parseInt(input2.value));
+});
+	async function f1() {
+
+		let res= await p1;
+		out.innerHTML=`${input1.value}.`;
+		input1.value='';
+	    input2.value="";
+}
+f1();
+	
+});
+
